58a5bd9f97a93d8e00772856228e78c8d536acd5
zhouhuang <zhmail0830@163.com>
2022-09-10 16:54:39 +0800

    add react-native-shadow & react-native-svg

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import {View, Image} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import {BoxShadow} from 'react-native-shadow';
+import {getShadowOpt} from '../../mock/shadowOpts';
 import style from './style';
 
 const NavHeader = ({navList}) => {
+  const shadowOpt = getShadowOpt({width: 750, height: 146});
   return (
-    <View style={style.headerWrapper}>
-      <LinearGradient
-        colors={['#FF5D79', '#FD003C']}
-        style={style.linearGradient}>
-        {navList.length
-          ? navList.map(item => (
-              <View style={style.iconWrapper} key={item.id}>
-                <Image style={style[item.styleName]} source={item.source} />
-                {item.isActive ? <View style={style.bar} /> : null}
-              </View>
-            ))
-          : null}
-      </LinearGradient>
-    </View>
+    <BoxShadow setting={shadowOpt}>
+      <View style={style.headerWrapper}>
+        <LinearGradient
+          colors={['#FF5D79', '#FD003C']}
+          style={style.linearGradient}>
+          {navList.length
+            ? navList.map(item => (
+                <View style={style.iconWrapper} key={item.id}>
+                  <Image style={style[item.styleName]} source={item.source} />
+                  {item.isActive ? <View style={style.bar} /> : null}
+                </View>
+              ))
+            : null}
+        </LinearGradient>
+      </View>
+    </BoxShadow>
   );
 };
 
